87c0bbc05dd7b1749602be2bb78d5e79a28538f3
Abiel MaGu <abielmagu@gmail.com>
2018-09-15 20:32:53 -0500

    Se agregaron comentarios de opciones para env

--- env.php
+++ env.php
@@ -1,10 +1,12 @@
 <?php 
 
 return [
-    'display_startup_errors' => 1,
-    'display_errors'         => 1,
-    'log_errors'             => 1,
-    'error_reporting'        => 1,
-    'memory_limit'           => '128M',
+    'display_startup_errors' => 1, // 
+    'display_errors'         => 1, // 0 - 1
+    'log_errors'             => 1, // 0 - 4
+    'error_reporting'        => E_ALL, // E_ALL | E_ERROR | E_WARNING | E_PARSE | E_NOTICE
     'timezone'               => 'America/Chicago'
+    //'memory_limit'           => '128M',
 ];
+
+// http://php.net/manual/en/errorfunc.configuration.php
