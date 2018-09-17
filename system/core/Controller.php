7d8e44eb4299860e88fca60f0fa11c3d344f1639
Abiel MaGu <abielmagu@gmail.com>
2018-09-17 15:25:34 -0500

    Update Controller.php
    Se agrego el metodo message para la variable session flash

--- system/core/Controller.php
+++ system/core/Controller.php
@@ -4,10 +4,19 @@ use System\Interfaces\iController;
 
 abstract class Controller extends Validator implements iController
 {
-    protected $request;
-
     public function __construct()
     {
         // code...
+    }
+    
+    protected function message($theme)
+    {
+        if( !is_null($theme) )
+        {
+            flash([
+                'color' => 'alert alert-'.$theme[0],
+                'message' => $theme[1]
+            ]);
+        }
     }
 }
