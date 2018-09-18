2cb0899b629fd1c478ba8506b5c328ea9b2baba1
Abiel MaGu <abielmagu@gmail.com>
2018-09-18 12:39:00 -0500

    Create Modeling.php
    Para instanciar un modelo variable, pasando la table como parametro de construccion

---
+++ system/core/Modeling.php
@@ -0,0 +1,11 @@
+<?php namespace System\Core;
+
+class Modeling extends Model
+{
+    protected $table;
+    
+    public function __construct($table = null)
+    {
+        $this->table = $table;
+    }
+}
