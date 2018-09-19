9a9158e28adc2545df08772a91b144dc6c6ba42d
Abiel MaGu <abielmagu@gmail.com>
2018-09-19 18:50:51 -0500

    Create validations.php

---
+++ config/validations.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'required' => 'Es requerido',
+    'string'   => 'Debe ser cadena de texto',
+    'email'    => 'Debe ser correo electronico',
+    'integer'  => 'Debe ser entero',
+    'max'      => 'Debe ser un maximo de',
+    'min'      => 'Debe ser un minimo de',
+    'unique'   => 'Debe ser único',
+];
