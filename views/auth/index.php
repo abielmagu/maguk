57f388809ca8be9aac669ec1626dbf94ccf1b0b5
Abiel MaGu <abielmagu@gmail.com>
2018-11-09 02:54:35 -0600

    Update index.php

--- views/auth/index.php
+++ views/auth/index.php
@@ -5,22 +5,22 @@
         <div class="card shadow">
             <div class="card-body">
                 <form action="<?= url('signing') ?>" method='post'>
-                    <p class="lead text-muted font-weight-bold">LA POTOSINA EXPRESS</p>
+                    <p class="lead text-muted font-weight-bold">APPNAME</p>
                     <div class="form-group">
-                        <input type="email" class="form-control" name="usuario" placeholder="Usuario" required>
+                        <input type="email" class="form-control" name="username" placeholder="Username" required>
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control" name="password" placeholder="Contraseña" required>
+                        <input type="password" class="form-control" name="password" placeholder="Password" required>
                     </div>
                     <p class="text-right m-0">
-                        <button type="submit" class="btn btn-primary">Iniciar sesion</button>
+                        <button type="submit" class="btn btn-primary">Sign in</button>
                     </p>
                 </form>
             </div>
             <div class="card-footer text-center">
-                <?php if( session_has('wrong_sign') ): ?>
-                    <span class="text-danger">Usuario o contraseña incorrectos</span>
-                <?php session_erase('wrong_sign'); endif ?>
+                <?php if( session_has('flash') && session_get('flash') === 'not_auth' ): ?>
+                    <span class="text-danger">Wrong username or password</span>
+                <?php session_erase('flash'); endif ?>
             </div>
         </div>
     </div>
