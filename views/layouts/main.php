1eba40ce6d3547978202289035982fee4b6e08fb
Abiel MaGu <abielmagu@gmail.com>
2018-11-09 03:18:20 -0600

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,22 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= asset('css/app.css') ?>">
     <title><?= config('app','name') ?></title>
 </head>
-<body>
-    <div class="container-fluid p-0">
+<body class="">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-2">
+            <div class="col-2 p-0 bg-light">
                 <?php $template->insert('layouts/navside') ?>
             </div>
-            <div class="col-sm">
+            <div class="col-sm py-3">
                 <?= $template->space('content') ?>
             </div>
         </div>
     </div>
+    
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/popper.js@1.14.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </body>
 </html>
