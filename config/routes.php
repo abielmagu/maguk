0323f4f099e9b316bd502602ce1e60152a344292
Abiel MaGu <abielmagu@gmail.com>
2018-11-09 00:35:36 -0600

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
+    // Auth
+    'sign' => 'AuthController:index',
+    'signing' => 'AuthController:signing',
+    'signout' => 'AuthController:signout',
+    
     // Dashboard
     'dashboard' => 'DashboardController:index',
     // 'dashboard/show/' = 'DashboardController:show' // Route with args like url "dashboard/show/{?}"
