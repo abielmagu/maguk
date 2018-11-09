94e0129f62de32621174e7764f89eebf116f4fca
Abiel MaGu <abielmagu@gmail.com>
2018-11-09 02:20:07 -0600

    Update alert.php

--- views/partials/alert.php
+++ views/partials/alert.php
@@ -1,6 +1,10 @@
 <?php if( session_has('flash') ): $flash = session_get('flash') ?>
+<?php if( isset($flash['message']) ): ?>
+
 <div class="alert <?= $flash['message']['color'] ?>">
     <b><?= $flash['message']['text'] ?></b>
 </div>
 <?php session_erase('flash') ?>
+
+<?php endif ?>
 <?php endif ?>
