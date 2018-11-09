b78a94e091c7db5086931628569638db03aabd13
Abiel MaGu <abielmagu@gmail.com>
2018-11-09 01:43:17 -0600

    Update Auth.php

--- models/Auth.php
+++ models/Auth.php
@@ -4,6 +4,19 @@ use System\Core\Model;
 
 class Auth extends Model
 {
-    protected $table = 'users';
+    static private $salt = 'salt_value';
+    protected $table = 'usuarios';
     protected $timestamps = true;
+
+    static public function hashToSign($value)
+    {
+        $value_salt = $value . self::$salt;
+        return hasher($value_salt);
+    }
+
+    static public function hashToCode($value)
+    {
+        $value_salt = $value . self::$salt;
+        return sha1($value_salt);
+    }
 }
