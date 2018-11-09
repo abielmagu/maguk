b792f36e2327eda53700cf4ea73b122ff4334475
Abiel MaGu <abielmagu@gmail.com>
2018-11-09 01:20:57 -0600

    Update index.php

--- views/auth/index.php
+++ views/auth/index.php
@@ -2,16 +2,16 @@
 
 <div class="row mt-4">
     <div class="col-sm col-sm-4 offset-sm-4 shadow rounded bg-white">
-        <form action="<?= url('signing') ?>" class="py-3" method='post'>
-            <p class="lead text-muted font-weight-bold">LA POTOSINA EXPRESS</p>
+        <form action="#" class="py-3" method='post'>
+            <p class="lead text-muted font-weight-bold">APPNAME</p>
             <div class="form-group">
-                <input type="text" class="form-control" name="usuario" placeholder="Usuario">
+                <input type="text" class="form-control" name="username" placeholder="Username">
             </div>
             <div class="form-group">
-                <input type="password" class="form-control" name="password" placeholder="Contraseña">
+                <input type="password" class="form-control" name="password" placeholder="Password">
             </div>
             <p class="text-right m-0">
-                <button type="submit" class="btn btn-primary">Iniciar sesion</button>
+                <button type="submit" class="btn btn-primary">Sign</button>
             </p>
         </form>
     </div>
