befd22b87ffbc87fab4d8e59317f549abbfc7b5c
Abiel MaGu <abielmagu@gmail.com>
2018-11-09 01:01:12 -0600

    Create index.php

---
+++ views/auth/index.php
@@ -0,0 +1,10 @@
+<?php $template->fill('content') ?>
+
+<div class="row">
+    <div class="col-sm col-sm-4 offset-sm-4">
+        <h1>Login</h1>
+    </div>
+</div>
+
+<?php $template->stop() ?>
+<?php $template->expand('layouts/login') ?>
