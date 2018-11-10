8a1e33a10788cdea88c7ea758eda3628c171b763
Abiel MaGu <abielmagu@gmail.com>
2018-11-10 17:24:40 -0600

    Update alert.php

--- views/partials/alert.php
+++ views/partials/alert.php
@@ -1,42 +1,42 @@
 <?php if( session_has('flash', 'message') ): $message = session_get('flash', 'message') ?>
 
-<?php if($messsage['status'] === 'info'): ?>
+<?php if($message['status'] === 'info'): ?>
 <div class="alert alert-info">
     <span class="alert-heading">Mensaje:</span>
     <span><?= $message['text'] ?></span>
 </div>
 
-<?php elseif($messsage['status'] === 'success'): ?>
+<?php elseif($message['status'] === 'success'): ?>
 <div class="alert alert-success">
     <span class="alert-heading">Mensaje:</span>
     <span><?= $message['text'] ?></span>
 </div>
 
-<?php elseif($messsage['status'] === 'warning'): ?>
+<?php elseif($message['status'] === 'warning'): ?>
 <div class="alert alert-warning">
     <span class="alert-heading">Mensaje:</span>
     <span><?= $message['text'] ?></span>
 </div>
 
-<?php elseif($messsage['status'] === 'danger'): ?>
+<?php elseif($message['status'] === 'danger'): ?>
 <div class="alert alert-danger">
     <span class="alert-heading">Mensaje:</span>
     <span><?= $message['text'] ?></span>
 </div>
 
-<?php elseif($messsage['status'] === 'dark'): ?>
+<?php elseif($message['status'] === 'dark'): ?>
 <div class="alert alert-dark">
     <span class="alert-heading">Mensaje:</span>
     <span><?= $message['text'] ?></span>
 </div>
 
-<?php elseif($messsage['status'] === 'secondary'): ?>
+<?php elseif($message['status'] === 'secondary'): ?>
 <div class="alert alert-secondary">
     <span class="alert-heading">Mensaje:</span>
     <span><?= $message['text'] ?></span>
 </div>
 
-<?php elseif($messsage['status'] === 'light'): ?>
+<?php elseif($message['status'] === 'light'): ?>
 <div class="alert alert-light">
     <span class="alert-heading">Mensaje:</span>
     <span><?= $message['text'] ?></span>
